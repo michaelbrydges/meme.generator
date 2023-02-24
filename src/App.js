0017ba22456e2f13739f41c7210ae2f48f26eb5a
michaelbrydges <michael.brydges@edu.sait.ca>
2023-02-24 16:30:35 -0700

    update structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import Meme from './components/Meme';
 
-function App() {
+export default function App() {
   return (
     <div className='main--card'>
       <Header />
@@ -10,5 +10,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
